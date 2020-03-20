2bb8a820ea84dab1e70a0004b4799079e9e3facf
Ale <alearanibar@gmail.com>
2020-03-20 08:08:23 -0700

    rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,15 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  
+  def generate(l, num):
+    if len(l) == n:
+      return [l]
+    else:
+      return generate(l + ['rock'], num) + generate(l + ['paper'], num) + generate(l + ['scissors'], num)
+  return generate([], n)
 
+rock_paper_scissors(3)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
